b2ac2810ab153000838277ea57e2b728b8e54edd
Ben Segarra <31346205+bfs1024@users.noreply.github.com>
2018-10-25 10:26:21 -0400

    tasklistScript
    tasklistScript using Rest call 
    ajax jquery

---
+++ taskListDataJS.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {    
+    loadItems();    
+});    
+    
+  
+function loadItems() {    
+    var siteUrl = "https://usff.navy.deps.mil/sites/netwarcom/N4-N7/KM/BenSandBox/_api/web/lists/getbytitle('TaskList')/items?$select=Title,JobNumber,Department,ProjectLead";
+   // var oDataUrl = siteUrl;   
+    $.ajax({    
+        url: siteUrl,    
+        type: "GET",    
+        dataType: "json",    
+        headers: {    
+            "accept": "application/json;odata=verbose"    
+        },    
+        success: mySuccHandler,    
+        error: myErrHandler    
+    });    
+}    
+  
+function mySuccHandler(data) 
+{    
+    try 
+	{    
+          
+        $('#table_id').DataTable(
+		{    
+              
+            "aaData": data.d.results,    
+            "aoColumns": [  
+            {    
+                "mData": "Title"    
+            },   
+            {    
+                "mData": "JobNumber"    
+            },   
+            {    
+                "mData": "Department"    
+            },
+			{    
+                "mData": "ProjectLead"    
+            }   
+                       
+            ]    
+        });    
+    } catch (e) {    
+        alert(e.message);    
+    }    
+}    
+    
+function myErrHandler(data, errMessage) {    
+    alert("Error: " + errMessage);    
+}  
